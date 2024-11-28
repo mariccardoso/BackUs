9d473076698d421497a6a5acff95cd9914b20fe9
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-11-28 16:59:04 -0300

    update: método post

--- Etapa 03/src/routes/cadastroRoutes.js
+++ Etapa 03/src/routes/cadastroRoutes.js
@@ -3,7 +3,28 @@ import { Router } from "express";
 const cadastroRoutes = Router();
 
 cadastroRoutes.post("/adicionar", (req, res) => {
+    try {
+        const { grupo, nome, apelido, senha } = req.body;
 
+        // Validações usando métodos da classe Aluno
+        const grupoValido = Aluno.prototype.validarGrupo(grupo);
+        const nomeValido = Aluno.prototype.validarNome(nome);
+        const apelidoValido = Aluno.prototype.validarApelidoNaoNulo(apelido);
+        const senhaValida = Aluno.prototype.validarSenha(senha);
+
+        // Verificar se já existe um aluno com a mesma senha
+        const alunoExistente = alunos.find((aluno) => aluno.pegarSenha() === senhaValida);
+        if (alunoExistente) {
+            return res.status(409).json({ message: "Já existe um aluno com essa senha!" });
+        }
+
+        const novoAluno = new Aluno(grupoValido, nomeValido, apelidoValido, senhaValida);
+        alunos.push(novoAluno);
+
+        res.status(201).json({ message: "Aluno adicionado com sucesso!", aluno: novoAluno });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 cadastroRoutes.get("/listar", (req, res) => {
