b9fa7a5710a3ed4c6080dfb3dc42412e1459b152
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-11-28 16:57:49 -0300

    feat: create delete method

--- Etapa 03/src/routes/cadastroRoutes.js
+++ Etapa 03/src/routes/cadastroRoutes.js
@@ -15,7 +15,15 @@ cadastroRoutes.put("/atualizar", (req, res) => {
 });
 
 cadastroRoutes.delete("/remover", (req, res) => {
+    const senha = req.params.senha; // Buscar aluno pela senha
+    const index = alunos.findIndex((aluno) => aluno.pegarSenha() === senha);
 
+    if (index === -1) {
+        return res.status(404).json({ message: "Aluno não encontrado!" });
+    }
+
+    alunos.splice(index, 1); // Remove o aluno da lista
+    res.status(200).json({ message: "Aluno removido com sucesso!" });
 });
 
 export default cadastroRoutes;
