f33251bacd19ffa62114cbbbf8ed6651dc1b96dd
maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2024-11-28 17:03:59 -0300

    feat: create get

--- Etapa 03/src/routes/cadastroRoutes.js
+++ Etapa 03/src/routes/cadastroRoutes.js
@@ -6,8 +6,10 @@ cadastroRoutes.post("/adicionar", (req, res) => {
 
 });
 
-cadastroRoutes.get("/listar", (req, res) => {
 
+// Get - Listar todos os alunos
+cadastroRoutes.get("/listar", (req, res) => {
+    res.status(200).send(alunos);
 });
 
 cadastroRoutes.put("/atualizar", (req, res) => {
